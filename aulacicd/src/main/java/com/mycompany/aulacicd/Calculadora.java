809a727e8054b903ecc7b89479e4b2f1ca76c825
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-10-11 10:28:32 -0300

    Criação do worklflow github

---
+++ aulacicd/src/main/java/com/mycompany/aulacicd/Calculadora.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aulacicd;
+
+/**
+ *
+ * @author Samsung Ocean
+ */
+
+public class Calculadora {
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicacao(int a, int b) {
+        return a * b;
+    }
+
+    public int divisao(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+}
+
+
